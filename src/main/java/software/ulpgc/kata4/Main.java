f30107db411138bf220a7d0c7ab83afb73728b79
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2023-12-31 00:21:49 +0000

    Main class modified to show histogram

--- src/main/java/software/ulpgc/kata4/Main.java
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -10,10 +10,17 @@ public class Main {
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:laptops.db")) {
             ComputerLoader computerLoader = new SqliteComputerLoader(connection);
             List<Computer> computers = computerLoader.loadAll();
-
+            double[] prices = new double[977];
+            int i = 0;
             for (Computer computer:computers) {
+                prices[i++] = Double.valueOf(computer.price())*0.000108;
                 System.out.println(computer);
             }
+            Histogram histogram = new Histogram("Prices of computers", "Prices in euros", "Number of computers", prices, 5);
+            MainFrame frame = new MainFrame();
+            frame.histogramDisplay().show(histogram);
+            frame.setVisible(true);
+
         }
     }
 }
